d689ae148a08fa2e4844d120ad694d82c46e64c5
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-09-28 17:18:25 -0300

    instalado firabase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCL1PDqeHH_B70jbFt0NvcESkBegJIDrp4",
+    authDomain: "bobbi-tea.firebaseapp.com",
+    projectId: "bobbi-tea",
+    storageBucket: "bobbi-tea.appspot.com",
+    messagingSenderId: "46233008056",
+    appId: "1:46233008056:web:2c671cbd169bd1fc10c14d"
+  };
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ export {firebase}
